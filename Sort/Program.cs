f533fbd5449b4063bc7b040e232f2b77946dbde1
Too.Shigeyuki <shigeyuki.to@techno-creative.co.jp>
2024-08-16 13:02:38 +0900

    ソートのリファクタリング

--- Sort/Program.cs
+++ Sort/Program.cs
@@ -3,29 +3,42 @@
 namespace Sort {
 
     /// <summary>
-    /// 
+    /// プログラム名：バブルソート演習
+    /// 担当者：戸尾
+    /// 作成日付：2024/08/17
     /// </summary>
     internal class Program {
+        /// <summary>
+        /// メインプログラム
+        /// </summary>
+        /// <param name="args">引数不要</param>
+        /// 
+        static readonly int[] list = new int[] { 4, 1, 3, 5, 2, 4, 5, 67, 89, 12, -2, 34, 12, -5 };
+
+        static Action<string> printm = (status) => { Console.Write("{0,-10}:", status); for (int i = 0; i < list.Length; i++) Console.Write("{0,3} ", list[i]); Console.Write("\n"); };
         static void Main(string[] args) {
 
-            int[] list = new int[] { 4, 1, 3, 5, 2,4,5,67,89,12,-2,34,12,-5 };
+            /*
+             * バブルソート本処理
+             * 　引数用途
+             *   
+             * setPointer:データを追い込む先のポインタ（小さい数字を左側に追い込む）
+             * swapPointer　：スワップ候補（右側）
+             * swapPointer-1：スワップ候補（左側）
+             */
 
-            for(int setP=0; setP <= list.Length; setP++) {
-                int swap2=list.Length - 1; ;
-                bool swapped = false;
-                for (int swap1=swap2-1; swap1 >= setP;swap2=(--swap1)+1) {
-                    if (list[swap2] < list[swap1]) {
-                        (list[swap1], list[swap2]) = (list[swap2], list[swap1]);
-                        swapped = true;
+            for (int setPointer=0; setPointer<list.Length-1;setPointer++) {                 //データ追い込み用ループ（左側に詰めながら追い込む
+                bool everswapped = false;                                                   //スワップを一回でも行えばtrue。一回もスワップしなかった場合false
+                for(int swapPointer=list.Length-1;swapPointer>setPointer;swapPointer--) {   //データスワップ用ループ
+                    if (list[swapPointer-1] > list[swapPointer]) {                          //スワップ候補（左側）よりスワップ候補（右側）が小さい場合                        (list[swapPointer-1], list[swapPointer]) = (list[swapPointer], list[swapPointer-1]);    //スワップ候補（左側）とスワップ候補（右側）をスワップ
+                        everswapped = true;                                                 //スワップを一回でも行った
                     }
                 }
-                if (!swapped) break;
+                if (!everswapped) break;                                                    //データ追い込み時に、一回でもスワップしなかった場合は、終了
+                printm("In Process");
             }
 
-            for(int i = 0; i<list.Length; i++) {
-                Console.Write("{0,3} ",list[i]);
-            }
-            Console.Write("\n");
+            printm("Finished");
 
             /*
              * --------------------------------------------------------
