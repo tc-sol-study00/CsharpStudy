be4529c5afd7076de73158fd021b274f7884a3dc
Too.Shigeyuki <shigeyuki.to@techno-creative.co.jp>
2024-08-27 10:53:56 +0900

    アップロード前ファイル名変更

---
+++ CSharpStudy202408/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Xml.Schema;
+
+namespace CSharpStudy20240825 {
+    internal class Program {
+
+        int GlobgalStoreData;
+
+        static void Main(string[] args) {       //ブロック
+
+            int a;
+            Console.WriteLine("Hello, World!");
+
+            const int maxlen = 3;
+
+            const float shohinzei = 1.08f;
+
+            int[] dtt = new int[] { 1, 2, 3 };
+
+            string dddd = "123123";
+
+            string cccc = "123";
+            cccc = "12313123";
+ 
+            a = 2;
+
+            string dt = "3";
+
+            while (true) {
+
+                int tempStoreData;
+
+                tempStoreData = 1;
+
+            }
+
+            int ifd = 0;
+
+            int[] datas = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int[] datas2;
+
+            foreach (int onedata in datas) {
+                Console.WriteLine(onedata);
+            }
+
+            for (int i = 0; i < datas.Length; i++) {
+                Console.WriteLine(datas[i]);
+            }
+
+            datas2 = datas;
+            datas[0] = 10;
+            Console.WriteLine(datas2[0]);
+
+            const int maxlen2 = 10;
+            int[] datalists = new int[maxlen2];
+
+            int bbb = 0x_ff_ff_ff;
+            bbb = 0b_11111111_11111111_11111111;
+
+            ulong usg=123ul;
+
+            double dv = 123e-100;  //123×10^-100   123×10^100
+
+            double y = method1(1);
+
+            method2("log1 処理開始");
+            Console.WriteLine("log1 処理開始");
+            Console.Write("log1 処理開始\n");
+            Console.Write("log1 処理\"開始\n");
+
+            Console.Write('\x0041');
+
+            method2("log1 処理開始");
+
+            double method1(double in1) {
+
+                double a = in1 * 1.08;
+                return a;
+            }
+
+            void method2(string in1) {
+                Console.WriteLine(in1);
+            }
+
+        }
+    }
+}
---
+++ ObjectOriented/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Xml.Linq;
+
+namespace ObjectOriented {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Dog dogA = new Dog("いぬＡ", 5);   //いぬＡの生成化・実態（インスタンシエーション）
+            Dog dogB = new Dog("いぬＢ", 20);   //いぬＢの生成化・実態（インスタンシエーション）
+
+            dogA.Gohan();
+            dogA.Gohan();
+            dogA.Gohan();
+
+            Console.WriteLine(dogA.leglength);
+
+            dogB.Gohan();
+            dogB.Gohan();
+
+            Console.WriteLine(dogA.leglength);
+
+            dogA.Naku();
+            dogB.Naku();
+
+            dogB.Gohan();
+            dogB.Gohan();
+            dogB.Gohan();
+            dogB.Gohan();
+
+            dogB.Naku();
+
+            SuperDog sdogC = new SuperDog("SいぬC", 100,0);
+            sdogC.Tobu(100);
+            sdogC.Tobu(100.0);
+            sdogC.Tobu();
+            sdogC.Tobu("100");
+
+            Console.WriteLine(sdogC.fliedDistance);
+
+            sdogC.Gohan();
+
+            Console.WriteLine(sdogC.leglength);
+            Console.WriteLine(sdogC.WingLength);
+
+            /*
+             * .    ピリオド
+             * ,    コンマ、カンマ
+             * :    コロン
+             * ;    セミコロン
+             */
+        }
+    }
+
+    public class Dog {  //スーパークラス・基底クラス・ベースクラス
+
+        //プロパティ
+        public string name { get; set; }
+        public double leglength { get; set; }
+
+        public string nakikata { get; set; } = "わんわん";
+
+        //コンストラクター
+        public Dog(string inName, double inLegLength) {
+            name = inName;
+            leglength = inLegLength;
+        }
+
+        //メソッド
+        public void Naku() {
+            Console.WriteLine(nakikata);
+        }
+
+        //
+        public double Gohan() {
+            leglength += 1;
+            if (leglength >= 25) {
+                nakikata = "バウワウ";
+            }
+            return leglength;
+        }
+    }
+    public class SuperDog : Dog { //サブクラス   //extents (クラス) implements(I/F)
+        public string name { get; set; }
+        public double fliedDistance { get; set; }
+
+        public double WingLength { get; set; } = 5;
+
+        public SuperDog(string inName, double inLegLength, double infliedDistance) : base(inName, inLegLength) {
+            fliedDistance = infliedDistance;
+            this.name = inName;
+            base.name = inName; //Javaの場合は、super
+        }
+
+        public void Tobu(double inTondaKyori) {
+            fliedDistance += inTondaKyori;
+        }
+
+        public void Tobu(int inTondaKyori) {    //オーバーロード
+            fliedDistance += inTondaKyori;
+        }
+
+        public void Tobu(string inStrTondaKyori) {    //オーバーロード
+            double inTondaKyori = int.Parse(inStrTondaKyori);
+            this.Tobu((int)inTondaKyori);               //Ｃａｓｔ（型変換）
+        }
+
+        public void Tobu() {    //オーバーロード
+            this.Tobu(5.0);
+        }
+
+        public double Gohan() { //オーバライド
+            WingLength += 1;
+
+            double leglength=base.Gohan();
+            return (leglength);
+        }
+
+    }
