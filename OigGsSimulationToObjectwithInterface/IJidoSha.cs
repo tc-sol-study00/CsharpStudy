d1edbf96d5c610fa17a44061add2e29e66083a48
Too.Shigeyuki <shigeyuki.to@techno-creative.co.jp>
2024-08-29 16:25:53 +0900

    インターフェース利用

---
+++ OigGsSimulationToObjectwithInterface/IJidoSha.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OigGsSimulationToObjectwithInterface {
+    public interface IJidoSha {
+        //メンバー（プロパティ）
+        public string CarName { get; set; }
+        public double NenPi { get; set; }
+        public double TankFull { get; set; }
+        public double TankLimit { get; set; }
+        public double TankZanryo { get; set; }
+        /*
+         * 走行
+         */
+        public (double, double) Soukou(int argSoukouKyori);
+        /*
+         * ガス残量チェック
+         */
+        public bool GsRemainCheck();
+        /*
+         * 家ーＧＳ間
+         */
+        public double RunBetweenHomeToGs(double argMovedDist);
+        /*
+         * ガス欠チェック
+         */
+        public bool GasKetsuCheck();
+    }
+}
